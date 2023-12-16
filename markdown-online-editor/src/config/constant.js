08ed4ec2dcc09ae64ef4438f5c685253b02d9eef
Levi <103307567+czy13724@users.noreply.github.com>
2023-12-16 15:00:01 +0800

    Update constant.js

--- markdown-online-editor/src/config/constant.js
+++ markdown-online-editor/src/config/constant.js
@@ -1,6 +1,7 @@
 /** @format */
+import exportToGitHub from '../utils/exportToGitHub';  // 替换成实际的路径
 
-export const appTitle = '在线 Markdown 编辑器'
+export const appTitle = '在线 Markdown 编辑器';
 
 export const exportTextMap = {
   '/export/png': '导出 PNG',
@@ -8,4 +9,11 @@ export const exportTextMap = {
   '/export/pdf': '导出 PDF',
   '/export/ppt': 'PPT 预览',
   '/export/md': '导出到GitHub并发表'
+};
+
+export async function handleExportToGitHub(markdownContent, accessToken, username, repoName, filePath) {
+  await exportToGitHub(markdownContent, accessToken, username, repoName, filePath);
+  // 这里可以添加一些用户反馈，例如通知用户导出成功等
+  console.log('Export to GitHub completed.');
 }
+
