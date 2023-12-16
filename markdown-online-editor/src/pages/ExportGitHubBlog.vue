b6e60db496a4c5b35d1164947056e8403891b134
Levi <103307567+czy13724@users.noreply.github.com>
2023-12-16 15:17:50 +0800

    Create ExportGitHubBlog.vue

---
+++ markdown-online-editor/src/pages/ExportGitHubBlog.vue
@@ -0,0 +1,24 @@
+<!-- ExportGitHubBlog.vue -->
+
+<template>
+  <div>
+    <button @click="exportToGitHub">导出到GitHub</button>
+  </div>
+</template>
+
+<script>
+import exportToGitHub from '@/utils/exportToGitHub'; // 替换成实际的路径
+
+export default {
+  methods: {
+    async exportToGitHub() {
+      const markdownContent = '...'; // 获取你的 Markdown 内容
+      const accessToken = 'your_default_access_token'; // 默认的 Access Token
+      const username = 'czy13724'; // 你的用户名
+      const repoName = 'czy13724.github.io'; // 你的仓库名
+
+      await exportToGitHub(markdownContent, accessToken, username, repoName);
+    },
+  },
+};
+</script>
